9f5f1c024171c0e765b15c201b72154f304b2509
DarielMalave <darielmalaveperez@gmail.com>
2022-03-26 15:38:45 -0500

    testing branch

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useRef } from 'react';
+import NavBar from './NavBar.js';
 import {v4 as uuid} from "uuid";
 
 function App() {
   return (
     <>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <NavBar />
     <h1>Hello world2</h1>
     </>
   );
--- fe/src/NavBar.js
+++ fe/src/NavBar.js
@@ -3,6 +3,11 @@ import React from 'react';
 export default function NavBar() {
     return(
         <>
+        <nav class="nav">
+            <a class="nav-link active" aria-current="page" href="#">PokeTypeRacer</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Link</a>
+        </nav>
         </>
     );
 }
