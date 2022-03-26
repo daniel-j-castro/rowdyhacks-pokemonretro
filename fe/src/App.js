c9e7bfafc5a3e09bf28ae9c60b3c05af2ca94e8f
DarielMalave <darielmalaveperez@gmail.com>
2022-03-26 15:31:13 -0500

    Added Bootstrap CDN; NavBar blank slate

--- fe/src/App.js
+++ fe/src/App.js
@@ -4,7 +4,8 @@ import {v4 as uuid} from "uuid";
 function App() {
   return (
     <>
-    <h1>Hello world</h1>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <h1>Hello world2</h1>
     </>
   );
 }
--- fe/src/index.js
+++ fe/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import './css/bootstrap.css';
+// import './js/bootstrap.js';
 
 ReactDOM.render(
   <React.StrictMode>
