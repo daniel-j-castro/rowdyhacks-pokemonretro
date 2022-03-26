67f402a645d371327d7602a1907db9760908008b
DarielMalave <darielmalaveperez@gmail.com>
2022-03-26 16:32:49 -0500

    Managed to get values (dynamic) for user keystrokes

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,13 +1,35 @@
 import React, { useState, useRef } from 'react';
 import NavBar from './NavBar.js';
+import Sprite from './Sprite.js'
+import charmander from './charmander.png';
 import {v4 as uuid} from "uuid";
 
 function App() {
+  const targetName = "charmander"; 
+
+  const [pokeName, setPokeName] = useState(targetName);
+  const [userInput, setUserInput] = useState("");
+  const textField = useRef();
+
+
+  // setItems(previousItems => {
+  //   return [...previousItems, {id: uuid(), name: itemName, completed: false}];
+  // });
+
+  function handleKeystroke(event) {
+    setUserInput(event.target.value);
+  }
+
+
+
   return (
     <>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
     <NavBar />
-    <h1>Hello world2</h1>
+    <Sprite image={charmander} />
+    <div>{targetName}</div>
+    <div>{userInput}</div>
+    <input type="text" ref={textField} onChange={handleKeystroke} value={userInput} />
     </>
   );
 }
--- fe/src/NavBar.js
+++ fe/src/NavBar.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function NavBar() {
     return(
         <>
-        <nav class="nav">
-            <a class="nav-link active" aria-current="page" href="#">PokeTypeRacer</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>
+        <nav className="nav">
+            <a className="nav-link active" aria-current="page" href="#">PokeTypeRacer</a>
+            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#">Link</a>
         </nav>
         </>
     );
