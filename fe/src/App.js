a2c4de2be9aaaad3b6de6109499b868198d290b7
aaron-gingrich <78753100+aaron-gingrich@users.noreply.github.com>
2022-03-26 16:29:37 -0500

    test

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <h1>Hello world2</h1>
+    <h1>Hello world 2</h1>
     </>
   );
 }
