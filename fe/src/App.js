f0dfeec11c04a1f7280a3c9b9d65d752142df866
DarielMalave <darielmalaveperez@gmail.com>
2022-03-26 17:30:35 -0500

    added logic to typing game (wrong character goes red, correct character goes green)

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import NavBar from './NavBar.js';
 import Sprite from './Sprite.js'
 import charmander from './charmander.png';
+import CorrectnessField from './CorrectnessField.js';
 import {v4 as uuid} from "uuid";
 
 function App() {
@@ -11,24 +12,20 @@ function App() {
   const [userInput, setUserInput] = useState("");
   const textField = useRef();
 
-
-  // setItems(previousItems => {
-  //   return [...previousItems, {id: uuid(), name: itemName, completed: false}];
-  // });
-
   function handleKeystroke(event) {
     setUserInput(event.target.value);
   }
-
-
 
   return (
     <>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
     <NavBar />
     <Sprite image={charmander} />
+    <br />
     <div>{targetName}</div>
-    <div>{userInput}</div>
+    <br />
+    <CorrectnessField targetString={targetName} userString={userInput} />
+    <br />
     <input type="text" ref={textField} onChange={handleKeystroke} value={userInput} />
     </>
   );
