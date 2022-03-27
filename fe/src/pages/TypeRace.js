4bbb9c110c591caeb3cd6013f5901ff4a0c6546c
DarielMalave <darielmalaveperez@gmail.com>
2022-03-27 11:27:42 -0500

    Managed to get random pokemon names and pokemon images to work now

--- fe/src/pages/TypeRace.js
+++ fe/src/pages/TypeRace.js
@@ -9,96 +9,94 @@ import dunsparce from '../components/TypeRace/dunsparce.png';
 import CorrectnessField from '../components/TypeRace/CorrectnessField';
 import {v4 as uuid} from "uuid";
 
-//const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
-
-// export default function App() {
-//   const [post, setPost] = React.useState(null);
-
-//   React.useEffect(() => {
-//     axios.get(baseURL).then((response) => {
-//       setPost(response.data);
-//     });
-//   }, []);
-
-//   if (!post) return null;
-
-//   return (
-//     <div>
-//       <h1>{post.title}</h1>
-//       <p>{post.body}</p>
-//     </div>
-//   );
-// }
-
-
 function TypeRace() {
+    // let baseURL = "/pokemon/";
+    // baseURL += Math.ceil(Math.random() * 890);
+    const [targetArray, setTargetArray] = useState([]);
+    const [post, setPost] = React.useState([]);
+    const [imagePost, setImagePost] = useState([]);
 
-    let baseURL = "https://retro-pokemon-game-api-k6cgale4bq-uc.a.run.app/pokemon/23";
-    //const newURL = baseURL += "1";
-    const [post, setPost] = React.useState(null);
 
     React.useEffect(() => {
-            axios.get(baseURL).then((response) => {
-                console.log(response.data[0].name);
-            setPost(response.data);
-        });
+            for (let i = 0; i < 5; i++) {
+                let baseURL = "/pokemon/";
+                baseURL += Math.ceil(Math.random() * 890);
+                axios.get(baseURL).then(response => {
+                    setPost(previousArray => {
+                        return [...previousArray, response.data.name];
+                    });
+                });
+                axios.get(baseURL).then(response => {
+                    const spriteURL = "https://projectpokemon.org/images/normal-sprite/" + response.data.image_name + ".gif";
+                    setImagePost(previousPost => {
+                        return [...previousPost, spriteURL];
+                    });
+                });
+            }
     }, []);
-
 
   // use AJAX/Axios to get random Pokemon names here
   // hardcoded five pokemon here in the meantime
   const targetNameArray = ["charmander", "bulbasaur", "totodile", "smeargle", "dunsparce"];
   const targetImageArray = [charmander, bulbasaur, totodile, smeargle, dunsparce];
-
+  //console.log(post);
+  //console.log(testArray);
   const [userInput, setUserInput] = useState("");
   const [userCount, setUserCount] = useState(0);
   const textField = useRef();
+  let trueArray;
+  let trueImageArray;
+    if (post.length === 5) {
+        trueArray = post;
+        trueImageArray = imagePost;
+
+        //console.log(trueArray);
+        //console.log(trueImageArray);
+    }
 
   function handleKeystroke(event) {
       setUserInput(event.target.value);
   }
 
   function checkIfComplete(event) {
-      if (event.keyCode === 13 && targetNameArray[userCount] === userInput) {
-          alert("Correct!");
+    if (event.keyCode === 13 && trueArray[userCount] === userInput) {
+        alert("Correct!");
 
-          // game completed here
-          if (userCount + 1 === targetNameArray.length) {
-              // at the moment, repeat over and over
-              // setUserCount(0);
-              setUserInput("Winner Winner Chicken Dinner");
-              textField.current.value = "Winner Winner Chicken Dinner";
-              alert("Congratulations, you won");
-              return;
-          }
+        // game completed here
+        if (userCount + 1 === post.length) {
+            setUserInput("Winner Winner Chicken Dinner");
+            textField.current.value = "Winner Winner Chicken Dinner";
+            alert("Congratulations, you won");
+            return;
+        }
 
-          setUserCount((previousCount) => {
-              return previousCount + 1;
-          })
-          setUserInput("");
-          textField.current.value = null;
-          }
-          else if (event.keyCode == 13 && targetNameArray[userCount] !== userInput) {
-              alert("Wrong");
-          }
+        setUserCount((previousCount) => {
+            return previousCount + 1;
+        })
+        setUserInput("");
+        textField.current.value = null;
+        }
+    else if (event.keyCode == 13 && trueArray[userCount] !== userInput) {
+        alert("Wrong");
     }
+  }
 
   return (
       <>
-      <div></div>
-      <Sprite image={targetImageArray[userCount]} />
+      {post.length === 5 ? 
+      <>
+      <Sprite image={trueImageArray[userCount]} />
       <br />
       {/* This will hold the target name the player needs to get */}
-      <div>{targetNameArray[userCount]}</div>
+      <div>{post[userCount]}</div>
       <br />
       {/* This field will hold the player's current progress (color coded) */}
-      <CorrectnessField targetString={targetNameArray[userCount]} userString={userInput} />
+      <CorrectnessField targetString={post[userCount]} userString={userInput} />
       <br />
       {/* This is the text field the player uses to type the targert name */}
       <input type="text" ref={textField} onChange={handleKeystroke} onKeyDown={checkIfComplete} autoFocus />
-
-      <br />
-      <br />
+      </> : <></>}
+      {/* onClick={(e) => { this.clickMe(e, someParameter); }}  */}
       </>
     );
 }
