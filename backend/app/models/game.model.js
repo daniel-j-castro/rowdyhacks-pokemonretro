fcf35da078c0bd1ccdab0e80ad5f8235db7c01c1
Daniel <djc.usa98@gmail.com>
2022-03-26 21:54:39 -0500

    fixed model

--- backend/app/models/game.model.js
+++ backend/app/models/game.model.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Pokemon', PokemonSchema);
+
+const GameSchema = mongoose.Schema({
+    stt_hs: [{
+        userId: Number
+    }],
+    tr_hs: [{
+        userId: Number
+    }]
+})
+
+module.exports = mongoose.model('Game', PokemonSchema);
