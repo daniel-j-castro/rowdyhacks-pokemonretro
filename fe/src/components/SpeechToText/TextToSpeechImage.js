851ff71e30aaf380f191e54dad25402b6ded9969
aaron-gingrich <78753100+aaron-gingrich@users.noreply.github.com>
2022-03-27 03:01:23 -0500

    added voice recon

--- fe/src/components/SpeechToText/TextToSpeechImage.js
+++ fe/src/components/SpeechToText/TextToSpeechImage.js
@@ -4,6 +4,106 @@ import ReactAudioPlayer from 'react-audio-player';
 
 export default function TextToSpeechImage() {
 
+var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+var SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+var phrases = [
+  'I love to sing because it\'s fun',
+  'where are you going',
+  'can I call you tomorrow',
+  'why did you talk while I was talking',
+  'she enjoys reading books and playing games',
+  'where are you going',
+  'have a great day',
+  'she sells seashells on the seashore'
+];
+
+    function testSpeech() {
+        
+        var phrase = phrases[0];
+        // To ensure case consistency while checking with the returned output text
+        phrase = phrase.toLowerCase();
+        
+      
+        var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
+        var recognition = new SpeechRecognition();
+        var speechRecognitionList = new SpeechGrammarList();
+        speechRecognitionList.addFromString(grammar, 1);
+        recognition.grammars = speechRecognitionList;
+        recognition.lang = 'en-US';
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+      
+        recognition.start();
+      
+        recognition.onresult = function(event) {
+          // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+          // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+          // It has a getter so it can be accessed like an array
+          // The first [0] returns the SpeechRecognitionResult at position 0.
+          // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+          // These also have getters so they can be accessed like arrays.
+          // The second [0] returns the SpeechRecognitionAlternative at position 0.
+          // We then return the transcript property of the SpeechRecognitionAlternative object 
+          var speechResult = event.results[0][0].transcript.toLowerCase();
+        
+          console.log(speechResult)
+      
+          console.log('Confidence: ' + event.results[0][0].confidence);
+        }
+      
+        recognition.onspeechend = function() {
+          recognition.stop();
+        }
+      
+        recognition.onerror = function(event) {
+          
+        }
+        
+        recognition.onaudiostart = function(event) {
+            //Fired when the user agent has started to capture audio.
+            console.log('SpeechRecognition.onaudiostart');
+        }
+        
+        recognition.onaudioend = function(event) {
+            //Fired when the user agent has finished capturing audio.
+            console.log('SpeechRecognition.onaudioend');
+        }
+        
+        recognition.onend = function(event) {
+            //Fired when the speech recognition service has disconnected.
+            console.log('SpeechRecognition.onend');
+        }
+        
+        recognition.onnomatch = function(event) {
+            //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+            console.log('SpeechRecognition.onnomatch');
+        }
+        
+        recognition.onsoundstart = function(event) {
+            //Fired when any sound — recognisable speech or not — has been detected.
+            console.log('SpeechRecognition.onsoundstart');
+        }
+        
+        recognition.onsoundend = function(event) {
+            //Fired when any sound — recognisable speech or not — has stopped being detected.
+            console.log('SpeechRecognition.onsoundend');
+        }
+        
+        recognition.onspeechstart = function (event) {
+            //Fired when sound that is recognised by the speech recognition service as speech has been detected.
+            console.log('SpeechRecognition.onspeechstart');
+        }
+        recognition.onstart = function(event) {
+            //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
+            console.log('SpeechRecognition.onstart');
+        }
+      }
+
+
+
+
     let [pokeAudio, setPokeAudio] = useState("");
     let [pokeSprite, setPokeSprite] = useState("");
     let [pokeName, setPokeName] = useState("");
@@ -35,8 +135,9 @@ export default function TextToSpeechImage() {
     function game (){
         
         console.log("GO!");
-    
+        testSpeech();
         setGameStates();
+        
 
         document.getElementById('audio').play();
 
@@ -141,6 +242,7 @@ export default function TextToSpeechImage() {
             <p>Wrong!</p>
         </div>
 
+        <button onClick={testSpeech}>Voice</button>
         
         </>
     );
